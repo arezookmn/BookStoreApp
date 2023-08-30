35e0343accdd6a57f70f5d423f83ab83eb2da040
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2023-08-30 13:37:15 +0330

    Add project files.

---
+++ BookStore.WebApi/Models/BookStoreAppDbContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.WebApi.Models;
+
+public partial class BookStoreAppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public BookStoreAppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public BookStoreAppDbContext(DbContextOptions<BookStoreAppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        => optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Authors__3214EC07E7617A19");
+
+            entity.Property(e => e.Bio).HasMaxLength(250);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Books__3214EC07ECFDB71A");
+
+            entity.HasIndex(e => e.Isbn, "UQ__Books__447D36EA5490EF71").IsUnique();
+
+            entity.Property(e => e.Image).HasMaxLength(250);
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(50)
+                .HasColumnName("ISBN");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)"); 
+            entity.Property(e => e.Summery).HasMaxLength(250);
+            entity.Property(e => e.Title).HasMaxLength(50);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books)
+                .HasForeignKey(d => d.AuthorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Books_ToTable");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
