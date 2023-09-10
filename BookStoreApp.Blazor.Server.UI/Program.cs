6a0a3b468498fc7302a83e303362c47cfca296f2
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2023-09-10 10:23:39 +0330

    Setup of blazor app , identity part

--- BookStoreApp.Blazor.Server.UI/Program.cs
+++ BookStoreApp.Blazor.Server.UI/Program.cs
@@ -1,6 +1,10 @@
-using BookStoreApp.Blazor.Server.UI.ApiServices.Base;
+using Blazored.LocalStorage;
 using BookStoreApp.Blazor.Server.UI.Data;
+using BookStoreApp.Blazor.Server.UI.Provider;
+using BookStoreApp.Blazor.Server.UI.Services.Authentication;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,9 +12,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddHttpClient<IClient, Client>(
-    cl => cl.BaseAddress = new Uri("https://localhost:7231")
-    );
+    cl => cl.BaseAddress = new Uri("https://localhost:7231"));
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(p => p.GetRequiredService<ApiAuthenticationStateProvider>());
+
+builder.Services.AddServerSideBlazor(options =>
+{
+    options.DetailedErrors = true; // Enable detailed error messages
+    options.JSInteropDefaultCallTimeout = TimeSpan.FromSeconds(30); // Set a timeout for JavaScript interop calls
+    //options = TimeSpan.FromSeconds(30); // Set a keep-alive interval
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,39 @@
+﻿using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+using BookStoreApp.Blazor.Server.UI.Provider;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IClient _client;
+        private readonly ILocalStorageService _localStorage;
+        private readonly ApiAuthenticationStateProvider _authenticationStateProvider;
+        public AuthenticationService(IClient client, ILocalStorageService localStorage, ApiAuthenticationStateProvider authenticationStateProvider)
+        {
+            _client = client;
+            _localStorage = localStorage;
+            _authenticationStateProvider = authenticationStateProvider;
+        }
+
+        public async Task<bool> AuthenticateAsync(LoginUserDTO loginUserDto)
+        {
+           var loginResponse = await _client.LoginAsync(loginUserDto);
+           if (loginResponse == null) return false;
+
+           //store token
+           await _localStorage.SetItemAsync("accessToken", loginResponse.Token);
+
+           //change auth state of app
+           await _authenticationStateProvider.LoggedIn();
+           return true;
+        }
+
+        public async Task Logout()
+        { 
+            await _authenticationStateProvider.Logout();
+
+        }
+    }
+}
